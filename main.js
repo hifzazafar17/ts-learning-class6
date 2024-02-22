722e9d7755560042f13c457e3617a0f4847e4180
Hifza Zafar <152292315+Hifza-Code-Journey@users.noreply.github.com>
2024-02-22 22:51:53 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+"use strict";
+// // syntax error 
+// lett mess ="hello";
+// // syntax error type errror
+// console.loger("hi");
+// // syntax error assignablity error
+// let a = 10 
+// a = "20";
+// strongly type syntax
+// let a:string = "Pakistan";
+// let d = 30;      // globle scope 
+// if (true){
+//     let a = 20;      // blog scope
+//     const b = 10     // blog scope
+//     var c = 30       //function scope 
+// }
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+//  hoisting search it at home
+// blog scope jb bhi hum na is ke blog ke agder banaye or agr is ke bhar assecc kr rhy ho tw nhi ho ga ya error de ga
+//  but var ma nhi aaye gay 
+// blog scope let or const 
+// function scope var
+// globel scope { jo humare pury program me ho or ya bhi error nhi de ga}
+// module
+// import { f,e,g,h } from "./service"     // simple export import
+// import z from "./service"
+// import hello from "./service"
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(e);
+// console.log(z);
+// console.log(hello);
---
+++ service.ts
@@ -0,0 +1,11 @@
+// export let f = 50;
+// export let e = 50;
+// export let h = 50;
+// export let g = 50;           // simple export
+
+// export { f,e,h,g };
+
+
+
+// let z = "hello";
+// export default z;
